764b1d8b8b02d488aafe9fdd00f757b148528881
peisenha <eisenhauer@policy-lab.org>
2019-04-08 16:21:10 +0200

    edits to lecture

--- lectures/02_potential_outcome_model/material/auxiliary.py
+++ lectures/02_potential_outcome_model/material/auxiliary.py
@@ -16,17 +16,21 @@ def plot_individual_specific_effects(with_parameters=None):
     
     ax.set_xlabel('$\delta$')
     ax.set_ylabel('Density')
-    ax.set_xticklabels(['', '', '', 0, '', '', ''])
+    ax.set_xticklabels(['', '', '', 0.5, '', '', ''])
     ax.set_xlim([-3, 3])
     ax.set_ylim([0, 0.5])
     
     ax.yaxis.set_major_locator(MaxNLocator(prune='both'))
+    pos = with_parameters
     
-    if with_parameters:
-        pos = with_parameters
+    if with_parameters and len(set(pos)) != 1:
         ax.axvline(x=pos[0], linewidth=3, label='ATE', color='red')
         ax.axvline(x=pos[2], linewidth=3, label='ATT', color='orange')
         ax.axvline(x=pos[1], linewidth=3, label='ATC', color='green')
+        ax.legend()        
+        
+    elif with_parameters and len(set(pos)) == 1:
+        ax.axvline(x=pos[0], linewidth=3, label='ATE = ATT = ATC', color='red')
         ax.legend()        
     
     
