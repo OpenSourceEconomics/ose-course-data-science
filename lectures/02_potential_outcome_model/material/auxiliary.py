956a9e2ae8a0b0ed058bed567d5782010f5c05e2
peisenha <eisenhauer@policy-lab.org>
2019-04-09 10:01:48 +0200

    removed code for exercises

--- lectures/02_potential_outcome_model/material/auxiliary.py
+++ lectures/02_potential_outcome_model/material/auxiliary.py
@@ -7,7 +7,6 @@ import numpy as np
 
 from matplotlib.ticker import MaxNLocator
 
-
 def plot_individual_specific_effects(with_parameters=None):
     fig, ax = plt.subplots(1, 1)
     x = np.linspace(-5, 5, 5000)
@@ -32,23 +31,3 @@ def plot_individual_specific_effects(with_parameters=None):
     elif with_parameters and len(set(pos)) == 1:
         ax.axvline(x=pos[0], linewidth=3, label='ATE = ATT = ATC', color='red')
         ax.legend()        
-    
-    
-    
-def get_illustrative_lalonde_data():
-    df = pd.read_pickle('../../datasets/processed/angrist_pischke/nswre74.pkl')
-
-    np.random.seed(123)
-    df = df[['treat', 're78']].sample(frac=1)
-    np.random.seed(None)
-    
-    is_treated = df['treat'] == 1
-
-    df['Y'] = df['re78']
-    df['Y_0'] = df.loc[~is_treated, 're78']
-    df['Y_1'] = df.loc[is_treated, 're78']
-
-    df['D'] = 0
-    df.loc[is_treated, 'D'] = 1
-
-    return df
