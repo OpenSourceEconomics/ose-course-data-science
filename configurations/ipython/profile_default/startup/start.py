2bd21511e1c222090f8484ad0b139f0689ebd85e
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2019-06-04 09:38:38 +0200

    edit to surpress warnings

--- configurations/ipython/profile_default/startup/start.py
+++ configurations/ipython/profile_default/startup/start.py
@@ -1,5 +1,9 @@
 from IPython import get_ipython
+import pandas as pd
+
 ipython = get_ipython()
 
 ipython.magic('load_ext autoreload')
 ipython.magic('autoreload 2')
+
+pd.options.mode.chained_assignment = None
