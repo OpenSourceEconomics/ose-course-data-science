81364340cb1422e63c966213e6cdc080bcdec7ff
Carolina Alvarez <carolina.alvarezg21@gmail.com>
2021-05-07 01:16:52 +0200

    remove import

--- lectures/potential-outcome-model/auxiliary.py
+++ lectures/potential-outcome-model/auxiliary.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import scipy.stats as ss
-import matplotlib.ticker as mticker
 from matplotlib.ticker import FixedLocator, FixedFormatter
 
 
