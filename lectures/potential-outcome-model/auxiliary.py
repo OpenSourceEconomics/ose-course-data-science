d012645b1bf33ecfaf4eb94b37598ae6df06a57d
Carolina Alvarez <carolina.alvarezg21@gmail.com>
2021-05-07 01:11:29 +0200

    fix aux

--- lectures/potential-outcome-model/auxiliary.py
+++ lectures/potential-outcome-model/auxiliary.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import scipy.stats as ss
+import matplotlib.ticker as mticker
 from matplotlib.ticker import FixedLocator, FixedFormatter
 
 
